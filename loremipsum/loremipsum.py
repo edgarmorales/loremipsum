35b54939d374a83359a12948e4c4283233465a2b
Edgar Morales <edgar.morales@gmail.com>
2020-05-28 21:00:56 -0400

    FEAT-001: Adding long loremipsum text generator.

--- loremipsum/loremipsum.py
+++ loremipsum/loremipsum.py
@@ -5,3 +5,17 @@ def generate_text():
     """
 
     return "Etiam auctor non urna sit amet posuere. Nunc tincidunt ornare laoreet."
+
+
+def generate_long_text():
+    """
+    Longer Lorem text.
+    """
+    return "Cras aliquet mollis ipsum, nec ullamcorper magna cursus sit amet. Nulla placerat orci tortor, vitae " \
+           "luctus odio laoreet a. Proin consectetur metus eget orci posuere, vitae laoreet arcu fermentum. Mauris " \
+           "neque dolor, dictum id purus semper, placerat feugiat nunc. Aliquam elementum sodales neque. Fusce augue " \
+           "ipsum, ultricies non nunc rhoncus, ultrices consequat justo. Quisque vitae rutrum sem. Etiam a nunc " \
+           "condimentum, euismod nisl dignissim, vulputate elit. Morbi id justo tristique dolor commodo pretium. " \
+           "Morbi non erat et risus pharetra vehicula eu vitae quam. Sed elementum erat vel augue bibendum semper. " \
+           "Sed euismod nibh a dignissim fermentum. Vestibulum ut augue vitae dolor tristique sodales. Etiam nec" \
+           " ultrices sapien, sit amet ultricies lorem."
